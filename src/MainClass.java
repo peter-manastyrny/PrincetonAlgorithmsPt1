44d89d67ba536fbd6c54a8bf814aa0e9c193d3d0
peter-manastyrny <manastyrny@gmail.com>
2014-06-18 14:29:05 +0200

    Initial commit

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,11 +1,19 @@
+import java.util.Scanner;
+
 /**
  * Created by MANASTYP on 18.06.14.
  */
 public class MainClass {
 
     public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter array size");
+        int N = sc.nextInt();
 
 
+
+        QuickFindUF quick = new QuickFindUF(N);
+        System.out.println(quick.toString());
 
 
     }
--- src/QuickFindUF.java
+++ src/QuickFindUF.java
@@ -5,16 +5,38 @@ public class QuickFindUF {
 
     private int[] id;
 
+    public QuickFindUF(int N) {
+        id = new int[N];
+        for (int i = 0; i < N; i++)
+            id[i] = i;
+    }
+
+    public String toString(){
+        int length = id.length;
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < length; i++ ){
+            result.append(id[i]);
+            result.append(" ");
+        }
+        return result.toString();
+    }
+
+    public boolean connected (int p, int q){
+        return id[p] == id[q];
+    }
+
+    public void union (int p, int q){
+        int pid = id[p];
+        int qid = id[q];
+        int length = id.length;
+
+        for  (int i = 0; i < length; i++ ){
+            if (id[i] == qid)
+                id[i] = pid;
+        }
 
 
-
-
-
-
-
-
-
-
-
-
+    }
 }
